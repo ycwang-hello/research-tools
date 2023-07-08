5a3d241ec49328fe4f0e989d99815f4c62a2d3e4
ycwang-hello <ycwang-hello@users.noreply.github.com>
2023-07-08 21:33:38 +0800

    latexport: supports config

--- LaTeX/README.md
+++ LaTeX/README.md
@@ -4,6 +4,7 @@ My LaTeX templates.
 - `template_A4 homework.tex`: An A4 template suitable for homework. An example explaining the usage is coming soon.
 
 ## latexport
+### Introduction
 When sharing or uploading your LaTeX documents to some websites, you may need to provide a minimum set of files (that can be complied, and no subdirectories). For example, you need to create a folder that contains a single main `.tex` file (without comments, `\input`, etc.), and the images included are also in the folder. `.cls`, `.bst`, `.bib`, `.bbl` should also be included if necessary. 
 
 Sometimes you may write different sections of your document in separate `.tex` files or store image files in different folders for clarity. Additionally, there may be images that are no longer needed in the document. This can make it inconvenient to prepare the minimum working set of files, and updating the set of files can be tiring if changes are made to the document later on.
@@ -18,7 +19,10 @@ Sometimes you may write different sections of your document in separate `.tex` f
 If you find errors or have suggestions, you can create an issue. You are also welcome to modify the script and contribute by creating a pull request.
 
 ### Example Usage
-This is an example of how the script can be used, although there are other ways to use it.
+This is an example of how the script can be used, although there are other ways to use it. To show the help message, run the script by:
+```
+python latexport.py -h
+```
 
 - Copy the `latexport.py` file to the directory where your main `.tex` file is located.
 - Open your terminal and go to the directory.
@@ -32,3 +36,15 @@ After running the script, you will find a file in the directory with the extensi
 python latexport.py -b
 ```
 This time, the bibliography is not merged, and the `.bst` file (if necessary) and the `.bib` files included by `\bibliography` are added into the zip file.
+
+### Configuration File
+You can make more settings with a configuration file. The simplest way is to run `latexport.py` once, and you will see a sample configuration file with the extension `.ltxpconfig` in the same directory of the main `.tex` file. The file will include explanations and examples.
+
+To manually add a file into the zip file, add this line to the configuration file:
+```
+add path/to/file  # filepath is RELATIVE TO THE MAIN TEX FILE.
+```
+To replace text in the LaTeX code, add this line to the configuration file:
+```
+replace <pattern> <replacement> # Replace <pattern> to <replacement> in the merged tex file. Regular expressions are supported. Here '.' matches any character INCLUDING A NEW LINE.
+```
--- LaTeX/latexport.py
+++ LaTeX/latexport.py
@@ -12,15 +12,35 @@ import glob
 from zipfile import ZipFile, ZIP_DEFLATED
 import subprocess
 
-parser = ArgumentParser()
+# default config file
+default_config = \
+r'''## The configuration file consists of lines of options.
+## A line typically starts with a command, followed by several parameters.
+## commands and parameters are seperated by whitespace characters (e.g. spaces).
+## If a parameter itself contains whitespace characters, there should be a backslash ('\') before each of the whitespace characters.
 
+## [add] manually adding files to the zip
+# add filepath # filepath is RELATIVE TO THE MAIN TEX FILE.
+
+## [replace] text replacement
+# replace <pattern> <replacement> # Replace <pattern> to <replacement> in the merged tex file. Regular expressions are supported. Here '.' matches any character INCLUDING A NEW LINE.
+# here are some examples:
+# replace \\added\{(?P<added_txt>.*?)\} \g<added_txt>  # \added{<added_txt>} -> <added_txt>
+# replace \\deleted\{(?P<deleted_txt>.*?)\}  # \deleted{<deleted_txt>} -> 
+# replace \\replaced\{(?P<old_txt>.*?)\}\{(?P<replaced_txt>.*?)\} \g<replaced_txt>  # \replaced{<old_txt>}{<replaced_txt>} -> <replaced_txt>
+# replace \\explain\{(?P<explain_txt>.*?)\}  # \explain{<explain_txt>} -> 
+'''
+
+# define arguments
+parser = ArgumentParser()
 parser.add_argument('file', nargs="?", default=None, help='The main tex file. If not given, automatically use the .tex file (if there is only one) in the current working directory.')
 parser.add_argument('-o', '--out', help='The name of the output zip file. The default is the same as that of the main tex file.')
 parser.add_argument('-p', '--params', default='', help='The parameters passed to latexpand')
 parser.add_argument('-b', '--bibtex', action='store_true', help='Use bibtex (with .bst, .bib) files. By default, bibliography is expanded using the *.bbl file.')
-
+parser.add_argument('-c', '--config', default=None, help='The configuration file. The default file is "<main tex file name>.ltxpconfig". If the config file does not exist, a sample file will be automatically generated.')
 args = parser.parse_args()
 
+# handle arguments
 if args.file is None:
     files = os.listdir('.')
     texfiles = [f for f in files if f.endswith('.tex')]
@@ -31,17 +51,38 @@ if args.file is None:
 
 if args.out is None:
     args.out = args.file[:-4] + '.merged.zip'
-
 if os.path.exists(args.out):
     overwrite = input(f'"{args.out}" already exists. Do you want to overwrite it? y/[n] >>> ')
     if overwrite not in ['y']:
         raise FileExistsError(f'"{args.out}" already exists. Nothing done.')
 
+if args.config is None:
+    args.config = args.file[:-4] + '.ltxpconfig'
+if not os.path.exists(args.config):
+    with open(args.config, 'w') as f:
+        f.write(default_config)
+with open(args.config) as f:
+    config = f.readlines()
+
+# expand bbl?
 if '--expand-bbl' not in args.params and not args.bibtex:
     bblfile = args.file[:-4] + '.bbl'
     if not os.path.exists(bblfile):
         raise FileNotFoundError(f"{bblfile} not found. You may consider passing `--bibtex` or `-p \"--expand-bbl BBLFILE\"` or generating a bbl file.")
     args.params += f' --expand-bbl {bblfile}'
+
+# parse config
+add_files = []
+replacements = []
+for line in config:
+    line = line.split('#')[0]
+    line = [re.sub(r'\\(\s)', r'\1', s) for s in re.split(r'(?<!\\)\s+', line)]
+    if line[0] == 'add':
+        add_files.append(line[1])
+        print(f"added file '{line[1]}'")
+    if line[0] == 'replace':
+        replacements.append((line[1], line[2]))
+        print(f"replace:  {line[1]}   ->   {line[2]}")
 
 output = subprocess.check_output(['latexpand'] + args.params.split() + [args.file])
 maintex = output.decode()
@@ -97,6 +138,14 @@ with ZipFile(args.out, 'w', compression=ZIP_DEFLATED) as f:
     # img file
     for i, imgpath in enumerate(imgpaths):
         move(imgpath, f, prefix=f'fig{i+1}_')
+
+    # added file in config
+    for add_file in add_files:
+        move(add_file, f)
+
+    # replacements in config
+    for pattern, repl in replacements:
+        maintex = re.sub(pattern, repl, maintex, flags=re.DOTALL)
     
     with f.open(os.path.basename(args.file), mode='w') as texf:
         texf.write(maintex.encode())
